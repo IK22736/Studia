116616b5f139c8312f80ad8190be2ed7313c34f2
IK22736 <igorigor0345@gmail.com>
2023-06-24 10:26:21 +0200

    add grade class

---
+++ src/main/java/com/studia/Grade.java
@@ -0,0 +1,35 @@
+package com.studia;
+
+public class Grade {
+
+    private String subject;
+    private Student student;
+    private Integer grade;
+
+    public Grade() {
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+}
